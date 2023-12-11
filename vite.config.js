95232597bc44f3342c3f98c8eba9cc89650f21d2
amdonatusprince <donatusprince@gmail.com>
2023-12-11 11:29:03 +0100

    deploy setup: error fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web3ridge-task"
+  base: "/web3ridge-task/"
 })
